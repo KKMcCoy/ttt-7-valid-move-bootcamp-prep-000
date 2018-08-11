5e43402608fd6d717816426eb6af951fa111df88
karen S <buzz.kyt@gmail.com>
2018-08-11 18:15:50 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,16 +1,19 @@
 def position_taken?(board, index)
  taken = nil;
+  
   if board[index] ==  " " || board[index] == "" || board[index] == nil  
   taken = false;
   else
   taken = true;
   end
+  
 end
   
 
 
 def valid_move?(board,index)
   valid = nil
+  
   if index.between?(0,8) = true && position_taken?(board, index) = false
   valid = true;
     
