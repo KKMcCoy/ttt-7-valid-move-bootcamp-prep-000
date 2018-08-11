14c99db955bfaae030c3311630ac938da0b87331
karen S <buzz.kyt@gmail.com>
2018-08-11 18:17:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,23 +1,22 @@
-def position_taken?(board, index)
- taken = nil;
+# def position_taken?(board, index)
+# taken = nil;
   
-  if board[index] ==  " " || board[index] == "" || board[index] == nil  
-  taken = false;
-  else
-  taken = true;
-  end
-taken
-end
-  
+#   if board[index] ==  " " || board[index] == "" || board[index] == nil  
+#   taken = false;
+#   else
+#   taken = true;
+#   end
+# taken
+# end
 
 
-def valid_move?(board,index)
-  valid = nil
+# def valid_move?(board,index)
+#   valid = nil
   
-  if index.between?(0,8) = true && position_taken?(board, index) = false
-  valid = true;
-  else 
-  valid = false;
-  end
-valid
-end
+#   if index.between?(0,8) = true && position_taken?(board, index) = false
+#   valid = true;
+#   else 
+#   valid = false;
+#   end
+# valid
+# end
